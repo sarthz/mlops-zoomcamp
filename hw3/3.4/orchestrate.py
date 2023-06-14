d03f823ec90694ff83af3a2ff3a89744e6349132
Sarvesh Thakur <sarveshthakur@Sarveshs-MacBook-Pro.local>
2023-06-14 12:21:13 -0400

    added artifact for q4 and additional data

--- hw3/3.4/orchestrate.py
+++ hw3/3.4/orchestrate.py
@@ -10,6 +10,9 @@ import mlflow
 import xgboost as xgb
 from prefect import flow, task
 
+from prefect.artifacts import create_markdown_artifact
+from datetime import date
+
 
 @task(retries=3, retry_delay_seconds=2)
 def read_data(filename: str) -> pd.DataFrame:
@@ -106,6 +109,23 @@ def train_best_model(
         mlflow.log_artifact("models/preprocessor.b", artifact_path="preprocessor")
 
         mlflow.xgboost.log_model(booster, artifact_path="models_mlflow")
+
+        markdown__rmse_report = f"""# RMSE Report
+        ## Summary
+
+        Duration Prediction 
+
+        ## RMSE XGBoost Model
+
+        | Region    | RMSE |
+        |:----------|-------:|
+        | {date.today()} | {rmse:.2f} |
+        """
+
+        create_markdown_artifact(
+            key="duration-model-report", markdown=markdown__rmse_report
+        )
+        
     return None
 
 
